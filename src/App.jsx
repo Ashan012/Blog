e08e45e61e8ab6ebbbd594faa8ba733c969ae2e9
ashan012 <khan89891918@outlook.com>
2025-09-21 19:38:17 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import conf from "./conf/conf";
+import { useDispatch } from "react-redux";
+import authServices from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  console.log(conf.appwriteUrl);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authServices
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  if (!loading) {
+    return (
+      <>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1 className="text-3xl font-bold underline"> world!</h1>
+      </>
+    );
+  }
 }
 
 export default App;
